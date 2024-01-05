1e5b272fa63f146bd8a75563f091cc7f404e1e89
El-gibbor <chiagoziem11@yahoo.com>
2024-01-05 22:03:02 +0400

    get employee data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,18 +2,20 @@
 """using JSONPlaceholder REST API to get information about an employees
 TODO list progress with his/her employee ID
 """
-import requests
-from sys import argv
 
-uri = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/'
-empl_data = requests.get(uri)
-td_data = requests.get(uri + 'todos')
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-td_completed = [td for td in td_data.json() if td.get('completed') is True]
+    uri = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/'
+    empl_data = requests.get(uri)
+    td_data = requests.get(uri + 'todos')
 
-dn = len(td_completed)
-td = len(td_data.json())
+    td_completed = [td for td in td_data.json() if td.get('completed') is True]
 
-print(f"Employee {empl_data.json().get('name')} is done with tasks {dn}/{td}:")
-for i in td_data.json():
-    print(f"\t {i.get('title')}")
+    dn = len(td_completed)
+    td = len(td_data.json())
+
+    print(f"Employee {empl_data.json().get('name')} is done with tasks {dn}/{td}:")
+    for i in td_data.json():
+        print(f"\t {i.get('title')}")
