8f44849a2ef836ee4ef77368f748564ea29b92eb
El-gibbor <chiagoziem11@yahoo.com>
2024-01-12 19:30:35 +0400

    export API data to json in a dictionary of list of dictionary format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,21 +9,23 @@ if __name__ == "__main__":
     from json import dump
 
     uri = f'https://jsonplaceholder.typicode.com/'
+
     u_data = requests.get(uri + 'users')
     td_data = requests.get(uri + 'todos')
 
     dictFormat = {}
 
     for ud in u_data.json():
-        user_todoList = [
-            {
-                'username': td.get('username'),
-                'task': td.get('title'),
-                'completed': ud.get('completed')
-            }
-            for td in td_data.json() if ud.get('id') == td.get('userId')
-        ]
-        dictFormat[ud.get('id')] = user_todoList
+        user_list = []
+        for td in td_data.json():
+            if ud.get('id') == td.get('userId'):
+                td_task = {}
+                td_task['task'] = td.get('title')
+                td_task['completed'] = td.get('completed')
+                td_task['username'] = ud.get('username')
+                user_list.append(td_task)
+        dictFormat[ud.get('id')] = user_list
 
     with open('todo_all_employees.json', 'w') as jsonFile:
         dump(dictFormat, jsonFile)
+
