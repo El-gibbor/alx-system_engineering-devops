21f8d0d178fcbdec6a40619141db7f4e79bff0f4
El-gibbor <chiagoziem11@yahoo.com>
2024-01-18 19:32:56 +0400

    recursive api call

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,16 @@
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     """ Recursively queries the Reddit API to retrieve titles of all hot
         articles in a given subreddit.
     Args:
         subreddit (str): The name of the subreddit to check.
-        hot_list (list): A list to store the titles of hot articles (default is an empty list).
-        after (str): Identifier for pagination, indicating the starting point for the next page.
+        hot_list (list): store the titles of hot articles.
+        after (str): pagination, starting point for the next page.
     Returns:
-        list or None: A list containing the titles of all hot articles for the given subreddit,
-                     or None if no results are found. """
+        list or None: contains titles of all hot articles for the
+        given subreddit,or None if no results are found. """
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
